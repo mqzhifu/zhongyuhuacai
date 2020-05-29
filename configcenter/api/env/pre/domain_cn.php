195d2841bcfa72e7fbcdb0ab4f0d44fd1f710181
mqzhifu <mqzhifu@sina.com>
2020-05-29 10:37:21 +0800

    a
    aaaa

--- configcenter/api/env/pre/domain_cn.php
+++ configcenter/api/env/pre/domain_cn.php
@@ -4,4 +4,4 @@ define('DOMAIN_URL', "api.900day.com");
 //静态文件域名
 define ('STATIC_URL', "static.900day.com");
 //http协议
-define ('HTTP:PROTOCOL', "https://");
+define ('HTTP_PROTOCOL', "https://");
--- configcenter/instantplayadmin/env/pre/domain_cn.php
+++ configcenter/instantplayadmin/env/pre/domain_cn.php
@@ -4,4 +4,4 @@ define('DOMAIN_URL', "admin.day900.com");
 //静态文件域名
 define ('STATIC_URL', "static.day900.com");
 //http协议
-define ('HTTP:PROTOCOL', "http://");
+define ('HTTP_PROTOCOL', "http://");
