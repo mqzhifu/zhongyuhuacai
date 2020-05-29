aaa32220e28b368fd554c72d004d93b3d14aeb8f
mqzhifu <mqzhifu@sina.com>
2020-05-29 16:25:57 +0800

    aa
    aaaa

--- configcenter/api/env/pre/domain_cn.php
+++ configcenter/api/env/pre/domain_cn.php
@@ -1,7 +1,7 @@
 <?php
 //网站域名
-define('DOMAIN_URL', "api.900day.com");
+define('DOMAIN_URL', "api.day900.com");
 //静态文件域名
-define ('STATIC_URL', "static.900day.com");
+define ('STATIC_URL', "api.day900.com");
 //http协议
 define ('HTTP_PROTOCOL', "https://");
