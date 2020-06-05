19d62835bf93d5f8857cead5897558c36edde439
mqzhifu <mqzhifu@sina.com>
2020-06-06 01:15:55 +0800

    dd
    dd

--- kernel/service/product.service.php
+++ kernel/service/product.service.php
@@ -4,10 +4,10 @@ class ProductService{
     public $limit = 10;
 
     const ORDER_TYPE =array(
-        1=> array('id'=>1,"name"=>'默认',"id"),
-        2=> array('id'=>2,"name"=>'新品',"id"),
-        3=> array('id'=>3,"name"=>'销量',"user_buy_total"),
-        4=> array('id'=>4,"name"=>'价格','lowest_price'),
+        1=> array('id'=>1,"name"=>'默认','field'=>"id"),
+        2=> array('id'=>2,"name"=>'新品','field'=>"id"),
+        3=> array('id'=>3,"name"=>'销量','field'=>"user_buy_total"),
+        4=> array('id'=>4,"name"=>'价格','field'=>'lowest_price'),
     );
 
 
