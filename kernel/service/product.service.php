d7faf88a49b569f75b8e0a10e02946ba4efacf1a
mqzhifu <mqzhifu@sina.com>
2020-07-06 18:43:13 +0800

    dd
    dddd

--- kernel/service/product.service.php
+++ kernel/service/product.service.php
@@ -200,7 +200,7 @@ class ProductService
 
                 var_dump($productCategoryAttrParaData);
                 foreach ($productCategoryAttrParaData as $k=>$v){
-                    foreach ($v as $k2=>$v2){
+                    foreach ($v['category_attr_para'] as $k2=>$v2){
                         foreach ($goods_low_price_row['pcap'] as $k3=>$v3){
                             if($v2['id'] == $v3['pcap_id']){
                                 var_dump($v2);
