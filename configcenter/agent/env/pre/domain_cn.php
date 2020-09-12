54618760c45498e945926c6335fba9ef3b04f481
mqzhifu <mqzhifu@sina.com>
2020-09-12 22:31:06 +0800

    d
    d

--- configcenter/agent/env/pre/domain_cn.php
+++ configcenter/agent/env/pre/domain_cn.php
@@ -1,7 +1,7 @@
 <?php
 //网站域名
-define('DOMAIN_URL', "api.day900.com");
+define('DOMAIN_URL', "agent.day900.com");
 //静态文件域名
-define ('STATIC_URL', "api.day900.com");
+define ('STATIC_URL', "static.day900.com");
 //http协议
 define ('HTTP_PROTOCOL', "https");
