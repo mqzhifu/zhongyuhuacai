2dc480c27ffa7edb10574d0255875ad84bce02d5
System Administrator <root@wangdongyandeMacBook-Pro.local>
2020-11-18 20:19:51 +0800

    ddd

--- kernel/service/product.service.php
+++ kernel/service/product.service.php
@@ -401,8 +401,9 @@ class ProductService
                 }
             }
             $realUrl = substr($realUrl, 0, strlen($realUrl) - 1);
-            $resize_tmp = explode(".",$realUrl);
-            $realUrl = $realUrl[0] . ".200x200.".$resize_tmp[1];
+            if(strpos($realUrl,'alicdn') !== false ){
+                $realUrl = substr($realUrl,0,strlen($realUrl-4)) . ".200x200.".substr($realUrl,strlen($realUrl-3));
+            }
             $row['pic'] = $realUrl;
         }
         return $row;
